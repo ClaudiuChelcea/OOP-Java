2837ececeb83456c4ef1ea6a7d73b35227822958
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-10-19 14:06:57 +0300

    Add lab2

---
+++ LAB2/task3/Point.java
@@ -0,0 +1,53 @@
+package lab2.task3;
+
+import java.io.Console;
+
+public class Point {
+
+    /* Fields */
+    private float x = 0f;
+    private float y = 0f;
+
+    /* Getters and setters */
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    /* Constructor */
+    Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /** Change the coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return nothing, just change the coordinates
+     */
+    public void changeCoords(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /* Display coordinates */
+    public void showCoordinates() {
+        System.out.printf("(%0.0d, %0.0d)\n", this.x, this.y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+}
---
+++ LAB2/task3/Polygon.java
@@ -0,0 +1,65 @@
+package lab2.task3;
+
+import lab12.reports.Pair;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Polygon {
+    /* Fields */
+    private int num_corners = 0;
+    Point[] corners;
+
+
+    /* Getters and setters */
+    public int getNum_corners() {
+        return num_corners;
+    }
+
+    public void setNum_corners(int num_corners) {
+        this.num_corners = num_corners;
+    }
+
+    /* Constructors */
+    Polygon(int num_corners) {
+        this.num_corners = num_corners;
+        corners = new Point[this.num_corners];
+        for(Point my_point : corners) {
+            my_point = new Point(0, 0);
+        }
+    }
+
+    Polygon(ArrayList<Integer> corners_coordinates) {
+        this(corners_coordinates.size());
+        for(int i = 0; i < this.num_corners; i = i + 2) {
+            if(corners[i/2] == null)
+                corners[i/2] = new Point(0,0);
+            corners[i / 2].changeCoords(corners_coordinates.get(i), corners_coordinates.get(i + 1));
+        }
+    }
+
+    /* Display polygon function */
+    public void displayME()
+    {
+        for(Point my_point : corners) {
+            if(my_point == null)
+                break;
+            System.out.print(my_point + " ");
+        }
+    }
+
+    /* Test */
+    public static void main(String[] args) {
+        /* Create array and add elements */
+        ArrayList<Integer> my_coordinates = new ArrayList<Integer>();
+        for(int i = 0; i < 10; ++i)
+            my_coordinates.add(i);
+
+        /* Create polygon */
+        Polygon my_poligon = new Polygon(my_coordinates);
+
+        /* Display polygon */
+        my_poligon.displayME();
+    }
+}
