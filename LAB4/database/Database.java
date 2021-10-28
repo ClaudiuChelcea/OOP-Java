80d35c483af6d8f2dea2c926d8bdedab0fe0f2f2
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-10-29 01:17:09 +0300

    add lab4

---
+++ LAB4/database/Database.java
@@ -0,0 +1,116 @@
+package LAB4.database;
+
+import LAB4.people.Student;
+import LAB4.people.Teacher;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/* Singleton pattern */
+public class Database {
+    private final List < Student > students = new ArrayList < > ();
+    private final List < Teacher > teachers = new ArrayList < > ();
+    public static Database instance;
+    private static int nr_instances;
+
+    static {
+        nr_instances = 0;
+        instance = null;
+    }
+
+    /* Constructor */
+    private Database() {
+        /* Can't create instance */
+    }
+
+    /* Return the instance of the singleton */
+    public static Database getDatabase() {
+        if (instance == null) {
+            instance = new Database();
+            nr_instances++;
+        }
+
+        return instance;
+    }
+
+    /* Get the number of instances: should be 0 or 1 */
+    public static int getNumberOfInstances() {
+        return nr_instances;
+    }
+
+    /* Add a teachers list to the current teachers list */
+    public void addTeachers(List < Teacher > teachers) {
+        this.teachers.addAll(teachers);
+    }
+
+    /* Add a students list to the current students list */
+    public void addStudents(List < Student > students) {
+        this.students.addAll(students);
+    }
+
+    /* Return all teachers that teach a cartain subject */
+    public List < Teacher > findTeachersBySubject(String subject) {
+        List < Teacher > wantedTeachers = new ArrayList < > () {};
+
+        for (var element: teachers) {
+            if (element.getSubjects().contains(subject))
+                wantedTeachers.add(element);
+        }
+        return wantedTeachers;
+    }
+
+    /* Students list getter */
+    public List < Student > findAllStudents() {
+        return students;
+    }
+
+    /* Teachers list getter */
+    public List < Teacher > findAllTeachers() {
+        return teachers;
+    }
+
+    /* Return all the students with a certain subject */
+    public List < Student > getStudentsBySubject(String subject) {
+        List < Student > wantedStudents = new ArrayList < > ();
+
+        for (var element: students) {
+            if (element.getSubjects().containsKey(subject))
+                wantedStudents.add(element);
+        }
+
+        return wantedStudents;
+    }
+
+    /* Return all students sorted by their average grade */
+    public List < Student > getStudentsByAverageGrade() {
+        List < Student > my_students = Database.getDatabase().findAllStudents();
+        my_students.sort(new Comparator < Student > () {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return (int)((o1.averageGrade() - o2.averageGrade()));
+            }
+        });
+
+        return my_students;
+    }
+
+    /* Return all students in a subject sorted by their grade on that subject */
+    public List < Student > getStudentsByGradeForSubject(String subject) {
+        List < Student > my_list = new ArrayList < > ();
+
+        for (var el: Database.getDatabase().findAllStudents()) {
+            if (el.getSubjects().containsKey(subject))
+                my_list.add(el);
+        }
+
+        my_list.sort(new Comparator < Student > () {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.getGradeForSubject(subject) - o2.getGradeForSubject(subject);
+            }
+        });
+
+        return my_list;
+    }
+}
---
+++ LAB4/people/Student.java
@@ -0,0 +1,94 @@
+package LAB4.people;
+
+import LAB4.database.Database;
+
+import java.util.*;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Map < String, Integer > subjects;
+
+    public Student(String firstName, String lastName, Map < String, Integer > subjects) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subjects = subjects;
+    }
+
+    public Student(Student other) {
+        this.firstName = new String(other.firstName);
+        this.lastName = new String(other.lastName);
+        this.subjects = new HashMap<>(other.subjects);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map < String, Integer > getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(HashMap < String, Integer > subjects) {
+        this.subjects = new HashMap<String, Integer>(subjects);
+    }
+
+    /* Find the average of the student's grades */
+    public double averageGrade() {
+        double average = 0;
+        int count_subjects = 0;
+        for (var el : this.subjects.entrySet()) {
+            average += el.getValue();
+            count_subjects++;
+        }
+
+        return average / (double) count_subjects;
+    }
+
+    /* Return the grade for a subject */
+    public int getGradeForSubject(String subject){
+        return this.subjects.get(subject);
+    }
+
+    /* Return all teachers */
+    public List < Teacher > getAllTeachers() {
+        return Database.getDatabase().findAllTeachers();
+    }
+
+    /* Return all teachers that teach a certain subject */
+    public List < Teacher > getTeachersBySubject(String subject) {
+        return Database.getDatabase().findTeachersBySubject(subject);
+    }
+
+    /* Return all students */
+    public List < Student > getAllStudents() {
+        return Database.getDatabase().findAllStudents();
+    }
+
+    /* Return all students that study a certain subject */
+    public List < Student > getStudentsBySubject(String subject) {
+        return Database.getDatabase().getStudentsBySubject(subject);
+    }
+
+    /* Return all students sorted by their average grade */
+    public List < Student > getStudentsByAverageGrade() {
+        return Database.getDatabase().getStudentsByAverageGrade();
+    }
+
+    /* Return all students in a subject sorted by their grade on that subject */
+    public List < Student > getStudentsByGradeForSubject(String subject) {
+        return Database.getDatabase().getStudentsByGradeForSubject(subject);
+    }
+}
